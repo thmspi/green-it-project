3efdc4a82c5c0d38a92cda687bd96c8c5700b7d6
thomas <amiscremail@gmail.com>
2025-04-25 15:09:29 +0200

    new db online

--- models/db.js
+++ models/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "imanou",
-  database: "todo_app",
+  host: "sql111.infinityfree.com",
+  user: "if0_38832491",
+  password: "VuxB88EhoP",
+  database: "if0_38832491_green_it",
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
