3c6cb0f3a677f30b06d579173cb66637eee8742f
MezziNour <134057629+MezziNour@users.noreply.github.com>
2025-04-28 19:46:08 +0200

    models/db.js

--- models/db.js
+++ models/db.js
@@ -1,14 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "sql111.infinityfree.com",
-  user: "if0_38832491",
-  password: "VuxB88EhoP",
-  database: "if0_38832491_green_it",
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: "localhost",
+  user: "root",
+  password: " ", // Mettre votre mdp SQL ici
+  database: "todo_app",
 });
 
 module.exports = pool;
